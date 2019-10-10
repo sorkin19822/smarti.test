03473312d49b02e81684c8855df4441e572b65e5
Aleksand Sorokin <frogsav@yandex.ru>
2019-10-10 22:44:51 +0300

    Реализована кнопка удаления элемента кроме последнего

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $( document ).ready(function() {
     $( ".addrule" ).click(function() {
-        fieldRule = $('.field:last').clone().appendTo('.field:last')
+        $(".field:last").clone().appendTo(".fields");
     });
+
 });
+
+function deleteField(elem) {
+    if($(".field").length>1) $(elem).closest(".field").remove()
+
+}
